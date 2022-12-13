dd61dd52a1d54a641c3623bda0799989b93f64a2
دانیال کفائی نژاد طهرانی <65863212+DaniyalKT@users.noreply.github.com>
2022-12-13 17:41:13 +0330

    update item list in view component

---
+++ src/app/views/item-list/item-list.component.ts
@@ -0,0 +1,10 @@
+import { Component, Input} from '@angular/core';
+
+@Component({
+  selector: 'app-item-list',
+  templateUrl: './item-list.component.html',
+  styleUrls: ['./item-list.component.css']
+})
+export class ItemListComponent {
+@Input() data:any[] = []
+}
