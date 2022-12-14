a4ad76f39fd22d0fc28f75814c4d4007db45d630
دانیال کفائی نژاد طهرانی <65863212+DaniyalKT@users.noreply.github.com>
2022-12-14 12:25:36 +0330

    update modsl

--- src/app/mods/modal/modal.component.ts
+++ src/app/mods/modal/modal.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-modal',
   templateUrl: './modal.component.html',
   styleUrls: ['./modal.component.css']
 })
-export class ModalComponent {
+export class ModalComponent implements OnInit {
+  
+  
+  constructor( private el: ElementRef){}
+  
+  ngOnInit(): void {
+      document.body.appendChild(this.el.nativeElement)
+  }
 
 }
